5b09409f344f7ece748c0572d19d562a17bd659c
Uladislau.Zh <uladislau.zh@gmail.com>
2024-10-29 22:20:34 +0300

    8kyu

---
+++ kyu 8/you-cant-code-under-pressure1.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// SOLUTION:
+
+function doubleInteger(i) {
+  return i * 2;
+}
+
+console.log(doubleInteger(2));
