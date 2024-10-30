6001b357170e4a8706626f4f6fa873e2826ebd04
Uladislau.Zh <uladislau.zh@gmail.com>
2024-10-30 22:36:54 +0300

    8kyu

---
+++ kyu 8/get-character-from-acsii-value.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+
+// SOLUTION:
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+console.log(getChar(55));
+console.log(getChar(64));
