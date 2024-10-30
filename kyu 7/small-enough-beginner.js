b491a9339fd37de310e43d8c51936486bb912b7d
Uladislau.Zh <uladislau.zh@gmail.com>
2024-10-30 22:30:45 +0300

    7kyu

---
+++ kyu 7/small-enough-beginner.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+// SOLUTION:
+
+function smallEnough(a, limit) {
+  return a.every((v) => v <= limit);
+}
+
+console.log(smallEnough([66, 101], 200));
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
